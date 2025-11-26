ad39c5462c255b2b520504b6b76ac978def9d7e8
Kerzinsin <kerrivera12@gmail.com>
2025-11-26 17:31:16 -0600

    Código de mapa mejorado

--- Proyecto II Intro/mapa.py
+++ Proyecto II Intro/mapa.py
@@ -1,3 +1,5 @@
+#Mapa
+
 import random
 from terreno import Camino, Muro, Tunel, Liana
 
@@ -5,13 +7,12 @@ class Mapa:
     def __init__(self, filas, columnas):
         self.filas = filas
         self.columnas = columnas
-        self.matriz = []  # Aquí se guardará la matriz del mapa
+        self.matriz = []
         self.pos_inicio = None
         self.pos_salida = None
         self.generar_mapa()
 
     def generar_mapa(self):
-        """Genera la matriz del mapa con casillas aleatorias."""
         tipos_casillas = [Camino, Muro, Tunel, Liana]
 
         self.matriz = [
@@ -19,22 +20,24 @@ class Mapa:
             for _ in range(self.filas)
         ]
 
-        # Establecer posición inicial y salida
         self.pos_inicio = (0, 0)
         self.pos_salida = (self.filas - 1, self.columnas - 1)
 
-        # Forzar que esas posiciones sean Camino
         self.matriz[0][0] = Camino()
         self.matriz[self.filas - 1][self.columnas - 1] = Camino()
 
     def mostrar_matriz(self):
-        """Muestra la matriz en texto (solo para pruebas)."""
         for fila in self.matriz:
             print("".join(c.simbolo for c in fila))
 
-# Prueba rápida
-if __name__ == "__main__":
-    mapa = Mapa(10, 15)
-    mapa.mostrar_matriz()
-    print(" Inicio:", mapa.pos_inicio)
-    print(" Salida:", mapa.pos_salida)
+    def mostrar_con_jugador_enemigo(self, jugador, enemigo=None):
+        for f in range(self.filas):
+            fila_str = ""
+            for c in range(self.columnas):
+                if (f, c) == jugador.obtener_posicion():
+                    fila_str += "J"
+                elif enemigo and (f, c) == enemigo.obtener_posicion():
+                    fila_str += "E"
+                else:
+                    fila_str += self.matriz[f][c].simbolo
+            print(fila_str)
