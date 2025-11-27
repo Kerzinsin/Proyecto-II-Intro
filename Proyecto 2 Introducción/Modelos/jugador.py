6b17f5f91affbd794d3a32ef5557e3376e951520
Kerzin <kerrivera12@gmail.com>
2025-11-27 10:55:05 -0600

    Implementación de nuevas mecánicas
    La función de correr y la energía

--- Proyecto 2 Introducción/Modelos/jugador.py
+++ Proyecto 2 Introducción/Modelos/jugador.py
@@ -1,12 +1,16 @@
 #Jugador
 
-from Modelos.terreno import Camino, Tunel
+import time
+from Modelos.terreno import Tunel
 
 class Jugador:
     def __init__(self, fila, columna):
         self.fila = fila
         self.columna = columna
         self.energia = 100
+        self.puede_correr = True
+        self.en_tunel = False
+        self.tiempo_en_tunel = None
 
     def obtener_posicion(self):
         return self.fila, self.columna
@@ -33,5 +37,34 @@ class Jugador:
             if self.puede_moverse(mapa, nueva_fila, nueva_columna):
                 self.fila = nueva_fila
                 self.columna = nueva_columna
+
+                casilla = mapa.matriz[nueva_fila][nueva_columna]
+                self._gestionar_energia(casilla)
                 return True
         return False
+
+    def correr(self, mapa):
+        if not self.puede_correr or self.energia < 100:
+            print("No tienes energía para correr.")
+            return False
+
+        self.energia = 0
+        self.puede_correr = False
+        print("Corriendo durante 0.5 segundos...")
+
+        time.sleep(0.5)
+        return True
+
+    def _gestionar_energia(self, casilla_actual):
+        if isinstance(casilla_actual, Tunel):
+            if not self.en_tunel:
+                self.en_tunel = True
+                self.tiempo_en_tunel = time.time()
+            else:
+                if time.time() - self.tiempo_en_tunel >= 1:
+                    self.energia = 100
+                    self.puede_correr = True
+                    print("Has recuperado energía completamente.")
+        else:
+            self.en_tunel = False
+            self.tiempo_en_tunel = None
